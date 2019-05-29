3f9d9e55a227a649ec4803bb70dad2265bdbeb2b
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-29 12:15:17 +0200

    error handling & get props user

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,19 +1,45 @@
 import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import { Redirect } from 'react-router-dom'
 
 class Dashboard extends PureComponent {
-    static propTypes = {
-        id: PropTypes.number,
-        name: PropTypes.string
-    }
-
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            id: null,
+            name: null,
+            redirect: false
+        }
+    }
+
+    static getDerivedStateFromProps(props, prevState) {
+        const { state } = props.location
+
+        if (state && state.id && state.name) {
+            return {
+                id: state.id,
+                name: state.name
+            }
+        }
+
+        return prevState
+    }
+
+    componentDidMount() {
+        if (!this.state.id || !this.state.name) {
+            this.setState({
+                redirect: true
+            })
+        }
     }
 
     render() {
+        const { redirect } = this.state
+
+        if (redirect) {
+            return <Redirect to='/'/>
+        }
+
         return (<></>)
     }
 }
