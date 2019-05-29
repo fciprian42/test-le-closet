aec28ccce586bfc367bb4f0d4a66d60ec10fadda
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-29 15:25:16 +0200

    trigger state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { middleWare, apiReducer } from "redux-rails";
+import { connectRoutes } from 'redux-first-router'
+import { createBrowserHistory } from 'history'
 import { apiConfig } from "../config";
 
 import sessionReducer from './reducers/sessionReducer'
 
+const history = createBrowserHistory()
+const routes = { home: '/', operators: '/operators', postes: '/postes', dashboard: '/dashboard/:id', login: '/login' }
+
+const { reducer } = connectRoutes(history, routes)
+
 const store = createStore(
-    combineReducers({session: sessionReducer, api: apiReducer(apiConfig)}),
+    combineReducers({location: reducer, session: sessionReducer, api: apiReducer(apiConfig)}),
   {},
   compose(applyMiddleware(middleWare(apiConfig)))
 );
