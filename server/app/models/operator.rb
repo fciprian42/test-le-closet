3e67ea3c15ade32f6fefefa685c35b793104516d
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-30 17:21:03 +0200

    update model operator

--- server/app/models/operator.rb
+++ server/app/models/operator.rb
@@ -2,7 +2,7 @@ class Operator < ApplicationRecord
   has_many :operators_postes, dependent: :destroy
   has_many :postes, through: :operators_postes
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :score, presence: true
 
   def name
     "#{first_name} #{last_name}"
@@ -10,7 +10,11 @@ class Operator < ApplicationRecord
 
   def to_builder
     Jbuilder.new do |operator|
-      operator.(self, :id, :name)
+      operator.(self, :id, :name, :score)
     end
+  end
+
+  def update
+    "#{score}"
   end
 end
