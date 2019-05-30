e2113740de9393b817a16d83bb24f6ae3b0b5c8c
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-30 14:45:43 +0200

    item & product models

--- server/app/models/item.rb
+++ server/app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   belongs_to :product
 
   validates :product, presence: true
+
+  def to_builder
+        Jbuilder.new do |item|
+          item.(self, :id, :id_product)
+        end
+      end
 end
--- server/app/models/product.rb
+++ server/app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :items, dependent: :destroy
+
+  def to_builder
+      Jbuilder.new do |product|
+        product.(self, :id, :name)
+      end
+    end
 end
