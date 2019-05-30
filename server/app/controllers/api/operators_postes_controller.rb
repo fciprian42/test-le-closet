dee33ff21b2d3d9b0aac1dc124eea8db926e257e
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-30 16:06:47 +0200

    operators postes controller (index & create method)

---
+++ server/app/controllers/api/operators_postes_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+end
+
+module Api
+    class OperatorsPostesController < ApplicationController
+         def index
+             sleep 1 # Simulate loading time
+             @operators_postes = OperatorsPostes.all
+         end
+
+         def create
+             @operator_poste = OperatorsPostes.new(operator_poste_params)
+             respond_to do |format|
+                 if @operator_poste
+                    format.json { render json: @operator_poste }
+                 end
+             end
+         end
+
+         private
+
+         def operator_poste_params
+             params.require(:operators_poste).permit(:operator_id, :poste_id, :operators_poste)
+         end
+    end
+end
