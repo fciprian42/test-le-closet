0debb271d9ad87d318d50353a0c64591e3bdb58e
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-30 13:10:09 +0200

    add folder actions & init Pickup component

---
+++ client/src/components/actions/Pickup.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+class Pickup extends PureComponent {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (<></>)
+    }
+}
+
+export default Pickup
