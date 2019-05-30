641a22891d2f113b1977a35424f853351fe9b295
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-31 00:51:14 +0200

    add route for item controller

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, constraints: { format: :json }, defaults: { format: :json } do
-    resources :operators, only: [:index, :update]
+    resources :operators, only: [:index, :update, :show]
     resources :products, only: [:index, :destroy]
     resources :postes, only: %i(index)
-    resources :items, only: [:index, :create]
+    resources :items, only: [:index, :create, :destroy, :update]
     resources :operators_postes, only: [:index, :create]
   end
 end
