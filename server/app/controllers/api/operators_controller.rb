9c3e6f850945c43223f6762a5fa9493d4d03bbf6
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-30 17:22:29 +0200

    operator update method

--- server/app/controllers/api/operators_controller.rb
+++ server/app/controllers/api/operators_controller.rb
@@ -1,8 +1,27 @@
+class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+end
+
 module Api
   class OperatorsController < ApplicationController
     def index
       sleep 1 # Simulate loading time
       @operators = Operator.all
     end
+
+    def update
+        @operator = Operator.find_by_id(params[:id])
+        if @operator.present?
+           @operator.score += 5
+
+           @operator.save
+
+            respond_to do |format|
+                format.json { render json: @operator }
+            end
+        end
+    end
+
+    private
   end
 end
