cf810f2e95131141f82e2699ac27db327181e887
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-30 14:18:27 +0200

    delete product method

--- server/app/controllers/api/products_controller.rb
+++ server/app/controllers/api/products_controller.rb
@@ -1,8 +1,16 @@
 module Api
   class ProductsController < ApplicationController
     def index
-      sleep 1 # Simulate loading time
-      @products = Product.all
+       sleep 1 # Simulate loading time
+       @products = Product.all
     end
+
+    def destroy
+       @product = Product.find(params[:id])
+       if @product.present?
+          @product.destroy
+       end
+    end
+
   end
 end
