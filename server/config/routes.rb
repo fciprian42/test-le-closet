3a4e70662a71d2fc2391ea46dbd4e64ec3a947db
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-30 14:17:28 +0200

    destroy producr

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, constraints: { format: :json }, defaults: { format: :json } do
     resources :operators, only: %i(index)
-    resources :products, only: %i(index)
+    resources :products, only: [:index, :destroy]
     resources :postes, only: %i(index)
   end
 end
