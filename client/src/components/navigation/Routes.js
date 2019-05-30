ce1571437cc47863e0bf6193ccf4f7d7fa146e9a
François Cipriani <francoiscipriani@MacBook-Pro-de-Francois.local>
2019-05-31 00:45:39 +0200

    add routes actions

--- client/src/components/navigation/Routes.js
+++ client/src/components/navigation/Routes.js
@@ -7,6 +7,8 @@ import Dashboard from "../Dashboard";
 import Login from "../Login";
 import Operators from "../Operators";
 import Pickup from "../actions/Pickup";
+import Checkup from "../actions/Checkup";
+import Packup from "../actions/Packup";
 import NotFound from "../NotFound";
 
 export default () => (
@@ -16,6 +18,8 @@ export default () => (
     <Route exact path="/login" component={Login} />
     <Route exact path="/operators" component={Operators} />
     <Route exact path="/pickup" component={Pickup} />
+    <Route exact path="/checkup" component={Checkup} />
+    <Route exact path="/packup" component={Packup} />
     <Route exact path="/postes" component={Postes} />
     <Route component={NotFound} />
   </Switch>
